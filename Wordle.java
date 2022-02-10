bcb2b610af4a6148eb3a4c153a6c19a0caede342
Andrea Leach <65141234+alleach24@users.noreply.github.com>
2022-02-09 20:40:47 -0500

    Update Wordle.java

--- Wordle.java
+++ Wordle.java
@@ -4,13 +4,41 @@
 */
 
 // import statements go here
+import java.io.*;
 
 public class Wordle {
    
    // main method
    public static void main(String[] args) {
       
-   }  
-  
+      // read in the list of all possible words that exist
+      // take in a file name from entered arguments
+      File file = new File(args[0]);
+      // create a new reader for the file
+      BufferedReader buffReader = new BufferedReader(new FileReader(file));
+      // initiate a string
+      String str;
+      // initiate a Set
+      Set<String> possilbeWords = new HashSet<String> ();
+      // read through all the strings in the file and add them to the set if they contain 5 letters
+      while ((str = buffReader.readLine()) != null) {
+         if (str.length() == 5) {
+            possibleWords.add(str);
+         }
+      }     
+      
+      
+      
+      
+   }
+   
+   
+   
+   // create a list of possible words
+   public Set<String> createPossilbeWords() {
+      
+      
+   }
+   
 }
 
