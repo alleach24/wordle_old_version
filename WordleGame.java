51f5d4b046156d11766e2bba4addf78910a17496
Andrea Leach <65141234+alleach24@users.noreply.github.com>
2022-02-11 00:01:52 -0500

    Create WordleGame.java

---
+++ WordleGame.java
@@ -0,0 +1,69 @@
+/* Created by: Andrea Leach
+   Date: 10 FEB 2022
+   Short Description: class that creates a Wordle Game
+   
+   Detailed Description: Detailed description goes here
+*/
+
+// import statements go here
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.*;
+
+public class WordleGame {
+   public Set<String> wordList;
+   public String answer;
+  
+   public static void main(String[] args) {
+      // CREATE THE FIRST LIST OF POSSIBLE LETTERS USING THE PROVIDED LIST
+      // initiate a new string to hold each word
+      String word;
+      // initiate a new Set to hold all the possible words
+      wordList = new HashSet<String>();
+      // create a keyboard scanner and prompt for the filename
+      Scanner keyboard = new Scanner(System.in);
+      System.out.print("Enter the filename for words to use: ");
+      String filename = keyboard.nextLine();
+      // create a file reader using the argument input
+      FileReader file = new FileReader(filename);
+      // create a buffered reader
+      BufferedReader br = new BufferedReader(file);
+      // run through each line of the file
+      ////////////////////////System.out.println("These are the possible words: ");
+      while ((word = br.readLine()) != null) {
+         // add the word to the Set of possible words if the word has 5 letters
+         if (word.length() == 5) {
+            wordList.add(word);
+            ////////////////////////System.out.println(word);
+         }
+      }
+     
+     
+     // take in the end word and set it as answer
+      System.out.println();
+      System.out.print("Enter the word to be optimized: ");
+      answer = keyboard.nextLine();
+      // check to make sure the answer is included in the provided list
+      while (true) {
+         if (!possibleWords.contains(answer)) {
+            System.out.print("This word is not valid. Please enter a different word: ");
+            answer = keyboard.nextLine();
+         }
+         else {
+            break; }
+      }
+   }
+  
+  
+  
+  public String getAnswer() {
+    return answer;
+  }
+  
+  public Set<String> getWordList() {
+    return wordList;
+  }
+  
+  
+}
